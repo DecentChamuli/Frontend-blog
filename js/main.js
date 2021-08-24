7a1166a9beda9f70830c5b8e1aa82aa56dd4c35f
Muhammad Tahir Ali <decentchamuli-cs@hotmail.com>
2021-08-24 23:19:00 +0500

    Fully Completed Frontend Blog

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const responsiveCarousel = {
+    0 : {
+        items : 1
+    },
+    320 : {
+        items : 1
+    },
+    560 : {
+        items: 2
+    },
+    960 : {
+        items: 3
+    }
+}
+
+
+$(document).ready(function(){
+    // Navbar Hamburger
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+    // Owl-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: responsiveCarousel 
+    });
+
+    // Move to Top
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop : 0
+        }, 1000);
+    })
+
+
+    AOS.init();
+});
